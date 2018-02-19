9b40cd90fe103bc57fe543de8544ef86d021fbcd
Nikhil Kolachalama <steinertprogramming@gmail.com>
2018-02-18 19:31:01 -0500

    finish two cube auto routines

--- src/org/usfirst/frc/team2180/robot/commands/TwoCubeAuto.java
+++ src/org/usfirst/frc/team2180/robot/commands/TwoCubeAuto.java
@@ -55,16 +55,70 @@ public class TwoCubeAuto extends CommandGroup {
     		} else if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'R') {
     			if (DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'L') {
         			// 1RL
+    				addParallel(new MoveElevator(10000)); //calculate precisely later
+    				addParallel(new OpenGrabber());
+    				addSequential(new RunForward(Robot.inchesToTicks(262.65)));
+    				addSequential(new Turn(45));
+    				addSequential(new RunForward(Robot.inchesToTicks(5)));
+    				addSequential(new DischargeCube());
+    				addParallel(new MoveElevator(-10000));
+    				addSequential(new Turn(-45));
+    				addSequential(new RunForward(-Robot.inchesToTicks(80)));
+    				addSequential(new Turn(90));
+    				addSequential(new RunForward(Robot.inchesToTicks(176.75)));
+    				addSequential(new Turn(90));
+    				addSequential(new RunForward(Robot.inchesToTicks(10)));
+    				addSequential(new CaptureCube());
+    				addSequential(new MoveElevator(2000));
+    				addSequential(new DischargeCube());
     			} else if (DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'R') {
     				// 1RR
+    				addSequential(new OneCubeAuto());
+    				addParallel(new MoveElevator(-2000));
+    				addSequential(new RunForward(-Robot.inchesToTicks(10)));
+    				addParallel(new RunForward(Robot.inchesToTicks(10)));
+    				addSequential(new CaptureCube());
+    				addSequential(new RunForward(-Robot.inchesToTicks(10)));
+    				addParallel(new MoveElevator(10000));
+    				addSequential(new Turn(-135));
+    				addSequential(new RunForward(Robot.inchesToTicks(90)));
+    				addSequential(new Turn(-135));
+    				addSequential(new DischargeCube());
     			}
     		}
     	} else if (position == 3) {
     		if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
     			if (DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'L') {
     				// 3LL
+    				addSequential(new OneCubeAuto());
+    				addParallel(new MoveElevator(-2000));
+    				addSequential(new RunForward(-Robot.inchesToTicks(10)));
+    				addParallel(new RunForward(Robot.inchesToTicks(10)));
+    				addSequential(new CaptureCube());
+    				addSequential(new RunForward(-Robot.inchesToTicks(10)));
+    				addParallel(new MoveElevator(10000));
+    				addSequential(new Turn(135));
+    				addSequential(new RunForward(Robot.inchesToTicks(90)));
+    				addSequential(new Turn(135));
+    				addSequential(new DischargeCube());
     			} else if (DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'R') {
     				// 3LR
+    				addParallel(new MoveElevator(10000)); //calculate precisely later
+    				addParallel(new OpenGrabber());
+    				addSequential(new RunForward(Robot.inchesToTicks(262.65)));
+    				addSequential(new Turn(-45));
+    				addSequential(new RunForward(Robot.inchesToTicks(5)));
+    				addSequential(new DischargeCube());
+    				addParallel(new MoveElevator(-10000));
+    				addSequential(new Turn(45));
+    				addSequential(new RunForward(-Robot.inchesToTicks(80)));
+    				addSequential(new Turn(-90));
+    				addSequential(new RunForward(Robot.inchesToTicks(176.75)));
+    				addSequential(new Turn(-90));
+    				addSequential(new RunForward(Robot.inchesToTicks(10)));
+    				addSequential(new CaptureCube());
+    				addSequential(new MoveElevator(2000));
+    				addSequential(new DischargeCube());
     			}
     		} else if (DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'R') {
     			if (DriverStation.getInstance().getGameSpecificMessage().charAt(1) == 'L') {
